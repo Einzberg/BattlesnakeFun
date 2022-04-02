cdf99add76f19b3a23a82c2f15c3f395842a0723
alec xu <alec_xu@hotmail.com>
2022-04-02 16:57:12 -0700

    make gradient for food

--- src/logic_gradient.py
+++ src/logic_gradient.py
@@ -1,7 +1,7 @@
 # import random
 # from typing import List, Dict
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 def get_info() -> dict:
     """
@@ -17,51 +17,83 @@ def get_info() -> dict:
         "head": "silly",  # TODO: Personalize
         "tail": "curled",  # TODO: Personalize
     }
+# Globals 
+food_weight = 5
+snake_weight = -9
+snake_head_weight = -2
+wall_weight = -9
 
-def gkern(l=10, sig=3., scale=10):
+board_x = None
+board_y = None
+
+def gkern(l=10, scale=10):
     """\
     creates gaussian kernel with side length `l` and a sigma of `sig`
     """
+    sig = (l-1)/3
     ax = np.linspace(-(l - 1) / 2., (l - 1) / 2., l)
     gauss = np.exp(-0.5 * np.square(ax) / np.square(sig))
     kernel = np.outer(gauss, gauss)
     return scale * kernel / np.max(kernel)
-  
+
 def centre_grad(data: dict) -> np.array:
-  board_w = data["board"]["width"] + 1
-  board_h = data["board"]["height"] + 1
+  board_w = data["board"]["width"]
+  board_h = data["board"]["height"]
   gradient_board = gkern(max(board_w, board_h))
-  populate_walls_on_grad(gradient_board, board_w, board_h)
 
   return gradient_board
 
-def populate_walls_on_grad(board: np.array, board_w, board_h):
-  for x in range(0, board_w): 
-    board[x, 0] = -9
-    board[x, board_h -1] = -9
+def populate_food(board: np.array, data: dict):
+  for food in data['board']['food']:
+    food_x, food_y = food['x'], food['y']
+    kern_size = max(11, 11)
+    kernel = gkern(kern_size, food_weight)
+    mid = kern_size // 2
+    pad_x  = mid - food_x
+    pad_y = mid - food_y
+    pad_x_tmp = None
+    pad_y_tmp = None
+    if pad_x > 0: 
+      pad_x_tmp = (0, pad_x)
+    else: 
+      pad_x_tmp = (-pad_x, 0)
+      
+    if pad_y > 0: 
+      pad_y_tmp = (0 , pad_y)
+    else: 
+      pad_y_tmp = (-pad_y, 0)
 
-  for y in range(0, board_h): 
-    board[0, y] = -9
-    board[board_w - 1, y] = -9
-
+    pad = (pad_x_tmp, pad_y_tmp)
+    print(food_x, food_y, pad)
+    kernel = np.pad(kernel, pad, "constant", constant_values=0)
+    if pad_x >= 0:
+      kernel = kernel[pad_x:,:]
+    else:
+      kernel = kernel[0:pad_x,:]
+    if pad_y >= 0:
+      kernel = kernel[:, pad_y:]
+    else:
+      kernel = kernel[:, :pad_y]
+    board += kernel
+    
 def populate_other_snakes(board: np.array, data: dict):
     for snake in data['board']['snakes']:        
         snake_body = snake['body']
         for ele in snake_body:
             if ele == snake['head']:
                 if ele == data['you']['head']: 
-                    board[ele['x'] + 1, ele['y'] + 1] = -9
+                    board[ele['x'], ele['y']] = snake_weight
                 elif snake['length'] < data['you']['length']:
                     continue
                 else:
-                    board[ele['x'] + 1, ele['y'] + 1] = -9
+                    board[ele['x'], ele['y']] = snake_weight
                     # direction snake head can go are dangerous
-                    board[ele['x'] + 1 + 1, ele['y'] + 1] += -2
-                    board[ele['x'] - 1 + 1, ele['y'] + 1] += -2
-                    board[ele['x'] + 1, ele['y'] + 1 + 1] += -2
-                    board[ele['x'] + 1, ele['y'] - 1 + 1] += -2
+                    board[ele['x'] + 1, ele['y']] += snake_head_weight
+                    board[ele['x'] - 1, ele['y']] += snake_head_weight
+                    board[ele['x'], ele['y'] + 1] += snake_head_weight
+                    board[ele['x'], ele['y'] - 1] += snake_head_weight
             else:
-                board[ele['x'] + 1, ele['y'] + 1] = -9
+                board[ele['x'], ele['y']] = snake_weight
 
 def follow_grad(head: dict, board: np.array) -> str:
     directions = {
@@ -82,12 +114,15 @@ def follow_grad(head: dict, board: np.array) -> str:
     return direction
 
 def choose_move(data: dict) -> str:
-    array_of_arrays = centre_grad(data)
+    board_y = data['board']['height']
+    board_x = data['board']['width']
+
+    board = centre_grad(data)
     # print(f'GRADIENT ARRAY: {array_of_arrays}')
 
-    populate_other_snakes(array_of_arrays, data)
-
-    direction = follow_grad(data['you']['head'], array_of_arrays)
+    populate_other_snakes(board, data)
+    board = np.pad(board, 1, 'constant', constant_values=wall_weight)
+    direction = follow_grad(data['you']['head'], board)
 
     # direction = follow_grad(array_of_arrays)
     print(f'GOING THIS DIRECTION: {direction}')
@@ -173,8 +208,12 @@ data = {
     }
 }
 
-# board = centre_grad(data)
-# populate_other_snakes(board, data)
+if True:
+  board = centre_grad(data)
+  board_x, board_y = 11, 11
+  populate_other_snakes(board, data)
+  populate_food(board, data)
+  board = np.pad(board, 1, 'constant', constant_values=snake_weight)
 
-# plt.imshow(np.rot90(np.fliplr(board)), interpolation='none', origin="lower")
-# plt.show()
+  plt.imshow(np.rot90(np.fliplr(board)), interpolation='none', origin="lower")
+  plt.show()
